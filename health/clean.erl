bd5f82548da4d1bde6c9fe68fd5e7594135c53fa
halfway258 <harancoh@gmail.com>
2023-04-28 18:09:54 +0300

    cleaned health line#

--- health/clean.erl
+++ health/clean.erl
@@ -1,12 +1,26 @@
-
 -module(clean).
 
 -export([start/1]).
 
 start(FName) ->
-    %% change ? => 0.0
     {ok, Data} = file:read_file(FName),
-    io:format("Old: ~p~n",[Data]),
+    % io:format("Old: ~p~n",[Data]),
+    %% change ? => 0.0
     NewData = string:replace(binary_to_list(Data), "?", "0.0", all),
-    io:format("New: ~p~n",[NewData]),
-    file:write_file(FName, NewData).
+    %% remove line numbers
+    NewerData = removeLineNum(NewData),
+    % io:format("New: ~p~n",[NewData]),
+    file:write_file(FName, NewerData).
+
+removeLineNum(Data) ->
+    Elements = string:split(Data, ",", all),
+    %% some elements will look like X\nNUM, need to replace with X\n
+    trimElem(Elements, []).
+
+trimElem([], Data) -> Data;
+trimElem([Elem | Elements], Data) ->
+    case string:find(Elem, "\n") of
+        nomatch -> trimElem(Elements, Data ++ Elem ++ ",");
+        _Normal -> trimElem(Elements, Data ++ string:left(Elem, 3) ++ "\n")
+    end.
+
